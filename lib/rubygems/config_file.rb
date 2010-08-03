17fde3c7a3250326ca9a86533d42beef56f46b9b
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2010-08-03 15:40:17 +0200

    Allow overriding system_config_path with the operating system defaults.
    If the Gem module has a system_config_path function, use that to report the
    system_config_path for the configuration file, this way operating systems
    that don't use /etc (such as Gentoo/Prefix that installs at arbitrary
    locations) can rewrite the path without having to patch more of Rubygems.

--- lib/rubygems/config_file.rb
+++ lib/rubygems/config_file.rb
@@ -46,24 +46,28 @@ class Gem::ConfigFile
   PLATFORM_DEFAULTS = {}
 
   system_config_path =
-    begin
-      require 'Win32API'
+    if Gem.respond_to? :system_config_path
+      Gem::system_config_path
+    else
+      begin
+        require 'Win32API'
 
-      CSIDL_COMMON_APPDATA = 0x0023
-      path = 0.chr * 260
-      if RUBY_VERSION > '1.9' then
-        SHGetFolderPath = Win32API.new 'shell32', 'SHGetFolderPath', 'PLPLP',
-                                       'L', :stdcall
-        SHGetFolderPath.call nil, CSIDL_COMMON_APPDATA, nil, 1, path
-      else
-        SHGetFolderPath = Win32API.new 'shell32', 'SHGetFolderPath', 'LLLLP',
-                                       'L'
-        SHGetFolderPath.call 0, CSIDL_COMMON_APPDATA, 0, 1, path
+        CSIDL_COMMON_APPDATA = 0x0023
+        path = 0.chr * 260
+        if RUBY_VERSION > '1.9' then
+          SHGetFolderPath = Win32API.new 'shell32', 'SHGetFolderPath', 'PLPLP',
+          'L', :stdcall
+          SHGetFolderPath.call nil, CSIDL_COMMON_APPDATA, nil, 1, path
+        else
+          SHGetFolderPath = Win32API.new 'shell32', 'SHGetFolderPath', 'LLLLP',
+          'L'
+          SHGetFolderPath.call 0, CSIDL_COMMON_APPDATA, 0, 1, path
+        end
+
+        path.strip
+      rescue LoadError
+        '/etc'
       end
-
-      path.strip
-    rescue LoadError
-      '/etc'
     end
 
   SYSTEM_WIDE_CONFIG_FILE = File.join system_config_path, 'gemrc'
